fa85cbbc3dac688ca17a6d83710f1bc168144418
Rodien_Bond004 <50739829+Rodien@users.noreply.github.com>
2023-07-09 20:17:02 +0200

    Initial commit

---
+++ BlazingFoodTestsAcceptance/DeleteMe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xunit;
+
+namespace BlazingFoodTestsAcceptance;
+public class DeleteMe
+{
+    [Fact]
+    public void Delete() =>
+    Assert.True(condition:true);
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BlazingFood
+ Blazor Playgrond 
