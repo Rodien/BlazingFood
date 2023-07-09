e730e71947102fcd057b8ec7d98da5d837f4aa20
Rodien_Bond004 <50739829+Rodien@users.noreply.github.com>
2023-07-09 22:03:34 +0200

    INFRA: Initialize Integration test
    Closes #3

---
+++ BlazingFoodTestsIntegration/DeleteMe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xunit;
+
+namespace BlazingFoodTestsIntegration;
+
+public class DeleteMe
+{
+    [Fact]
+    public void DeleteMe_Integration_ShouldBeTrue() =>
+            Assert.True(condition:true); 
+}
