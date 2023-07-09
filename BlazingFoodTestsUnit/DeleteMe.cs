74dc99af9bfebed362d9a2d3ed0713d66d0d3723
Rodien_Bond004 <50739829+Rodien@users.noreply.github.com>
2023-07-09 21:08:38 +0200

    INFRA: Initialize Unit Test

---
+++ BlazingFoodTestsUnit/DeleteMe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Xunit;
+
+namespace BlazingFoodTestsUnit;
+
+public class DeleteMe
+{
+    [Fact]
+    public void DeleteMe_ShouldBeTrue() =>
+        Assert.True(condition: true);
+}
